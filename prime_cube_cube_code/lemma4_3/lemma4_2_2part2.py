40205f953d0dbcae4dedf7b75419f7fe8bf38037
Kenny Applegate <kapplegate2020@gmail.com>
2026-06-03 20:39:56 -0400

    changed names of things to match lemma4_2

--- prime_cube_cube_code/lemma4_3/lemma4_2_2part2.py
+++ prime_cube_cube_code/lemma4_3/lemma4_2_2part2.py
@@ -1,5 +1,23 @@
-possibleBadNs = list(map(int, open("logs/possExceptions.txt", "r").read().split()))
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+import helperFunctions
 
-with open("possibleBadNs.txt", "w") as f:
-    for x in sorted(possibleBadNs):
+possible_ns = list(map(int, open("logs/possExceptions.txt", "r").read().split()))
+
+
+with open("possible_ns.txt", "w") as f:
+    for x in sorted(possible_ns):
+        f.write(f"{x}\n")
+
+
+
+definite_ns = set()
+for x in possible_ns:
+    if not helperFunctions.getCubeCubePrime(x):
+        if helperFunctions.verifyException(x):
+            definite_ns.add(x)
+
+with open("definite_ns.txt", "w") as f:
+    for x in sorted(definite_ns):
         f.write(f"{x}\n")
